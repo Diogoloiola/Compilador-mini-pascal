fefa168f22d5805524284606be39bf4290b72dff
diogoloiola <diogoloiola666@gmail.com>
2021-02-05 15:10:31 -0300

    adicionada as palavras reservadas

---
+++ source/palavrasLinguagem.py
@@ -0,0 +1,48 @@
+tipos = {}
+operadores = {}
+palavrasReservadas = {}
+
+tipos['char'] = 'Char'
+tipos['real'] = 'Real'
+tipos['string'] = 'String'
+tipos['boolean'] = 'Boolean'
+tipos['integer'] = 'Integer'
+
+operadores['or'] = 'Or'
+operadores['and'] = 'And'
+operadores['not'] = 'Not'
+operadores['xor'] = 'Xor'
+operadores['shl'] = 'Shl'
+operadores['shr'] = 'Shr'
+operadores['div'] = 'Div'
+operadores['mod'] = 'Mod'
+
+palavrasReservadas['program'] = 'Program'
+palavrasReservadas['function'] = 'Function'
+palavrasReservadas['procedure'] = 'Procedure'
+palavrasReservadas['forward'] = 'Forward'
+
+palavrasReservadas['begin'] = 'Begin'
+palavrasReservadas['end'] = 'End'
+
+palavrasReservadas['of'] = 'Of'
+palavrasReservadas['var'] = 'Var'
+palavrasReservadas['array'] = 'Array'
+palavrasReservadas['const'] = 'Const'
+
+palavrasReservadas['if'] = 'If'
+palavrasReservadas['then'] = 'Then'
+palavrasReservadas['else'] = 'Else'
+palavrasReservadas['case'] = 'Case'
+palavrasReservadas['for'] = 'For'
+palavrasReservadas['to'] = 'To'
+palavrasReservadas['downto'] = 'DownTo'
+palavrasReservadas['do'] = 'Do'
+palavrasReservadas['while'] = 'While'
+palavrasReservadas['repeat'] = 'Repeat'
+palavrasReservadas['until'] = 'Until'
+palavrasReservadas['write'] = 'write'
+
+CONSTANTE_NUMEROS = '0123456789'
+CONSTANTE_NUMEROS_HEXADECIMAL = '0123456789abcdef'
+IDENTIFICADOR_CONSTANTE = '_abcdefghijklmnopqrstuvwxyz'
