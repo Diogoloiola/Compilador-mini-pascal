4c38a10124f2a0eff7cf5e9a222ba48a2f4e1c44
gustavo-wanderley <gustavorw03@gmail.com>
2021-02-05 19:10:32 -0300

    add arquivo main.pas

---
+++ main.pas
@@ -0,0 +1,27 @@
+program exemplo5;
+var
+  n,k       : integer;
+  f1,f2,f3  : integer;
+procedure findMin;
+var novo2: integer;
+begin
+  
+end;
+begin
+  findMin();
+  n := 10 +  10 * 10 / 10;
+  n := 20;
+
+  if 10 <> 10 then
+    write('o numero e ',10,10);
+  else
+    write('o numero e ',10,10);
+
+  while k<=n do
+  begin
+    f3 := f1 + f2;
+    f1 := f2;
+    f2 := f3;
+    k := k + 1
+  end;
+end.
