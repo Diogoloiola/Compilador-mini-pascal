3a4f9dc8eb962ef4609b34d8bacc0cd1e643dc0e
gustavo-wanderley <gustavorw03@gmail.com>
2021-02-15 19:02:24 -0300

    add variaveis necessarias, e def gettoken e fimdearquivo

--- sintatico.py
+++ sintatico.py
@@ -5,9 +5,26 @@ class AnalisadorSintatico(object):
         self.indice = 0
         self.tabela = []
         self.tabela = tabelaToknes
+        '''
+        self.inicio = 0
+        self.fim = 0
+        self.cont = 0
+        self.eAtribuicao = False
+        self.eAtribuicaoTipoVariavel = False
+        self.variaveisDaFuncoes = {}
+        self.nomeFuncaoProcedimento = '' 
+        self.declaracaoFuncoesProcedimento = []
+        '''
 
     def proximoElemento(self):
         self.indice += 1
     
     def voltar(self):
         self.indice -= 1
+    
+    def getToken(self):
+        if not self.fimArquivo():
+            return self.tabela[self.indice][1]
+     
+    def fimArquivo(self):
+        return self.indice == len(self.tabela)
