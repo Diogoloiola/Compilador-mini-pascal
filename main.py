d8cfc6c56e3d9c04eaee11199aef005726d2f1cf
diogoloiola <diogoloiola666@gmail.com>
2021-03-03 21:42:39 -0300

    atualizado main, pq nao tinham comitado

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from typing import Text
 import sys
 from lexico import analisadorLexico
+from sintatico import AnalisadorSintatico 
 
 if __name__ == "__main__":
    lexico = analisadorLexico('main.pas')
    lexico.criaTabela()
+   sintatico = AnalisadorSintatico(lexico.tabelaToknes)
+
+   flag = sintatico.valida()
+   print(flag)
